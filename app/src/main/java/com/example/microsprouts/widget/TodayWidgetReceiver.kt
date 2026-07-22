efbca29c8c08a97633a615b5b25aef174f778563
Ann <ann.moormann@gmail.com>
2026-07-22 16:48:24 -0400

    widget is working

---
+++ app/src/main/java/com/example/microsprouts/widget/TodayWidgetReceiver.kt
@@ -0,0 +1,8 @@
+package com.example.microsprouts.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class TodayWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TodayWidget()
+}
